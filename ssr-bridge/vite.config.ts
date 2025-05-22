a4b24a55863740d9f298bbc7c6f22130571fb106
justinvdm <justinvderm@gmail.com>
2025-05-22 15:02:30 +0200

    progress

--- ssr-bridge/vite.config.ts
+++ ssr-bridge/vite.config.ts
@@ -1,6 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 import bridgePlugin from "./config/bridgePlugin.mts";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   environments: {
@@ -8,17 +12,17 @@ export default defineConfig({
     rsc: {},
   },
   plugins: [
-    cloudflare({}),
+    cloudflare({
+      configPath: path.resolve(__dirname, "config", "ssrDevWrangler.jsonc"),
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
     cloudflare({
       viteEnvironment: {
         name: "rsc",
       },
     }),
     bridgePlugin(),
-    //cloudflare({
-    //  viteEnvironment: {
-    //    name: "ssr",
-    //  },
-    //}),
   ],
 });
