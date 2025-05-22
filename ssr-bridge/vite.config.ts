e89647ddf0a9c3e81f1879f984bb35052fa0e011
justinvdm <justinvderm@gmail.com>
2025-05-22 11:29:48 +0200

    progress

--- ssr-bridge/vite.config.ts
+++ ssr-bridge/vite.config.ts
@@ -1,3 +1,21 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  environments: {
+    ssr: {},
+    rsc: {},
+  },
+  plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: "rsc",
+      },
+    }),
+    //cloudflare({
+    //  viteEnvironment: {
+    //    name: "ssr",
+    //  },
+    //}),
+  ],
+});
---
+++ ssr-bridge/vite/bridgePlugin.mts
@@ -0,0 +1,7 @@
+import type { Plugin } from "vite";
+
+export default function bridgePlugin(): Plugin {
+  return {
+    name: "ss-rsr-bridge",
+  };
+}
