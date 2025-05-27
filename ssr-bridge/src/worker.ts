6e95060217da870350ffc72803c5b91c2516e275
justinvdm <justinvderm@gmail.com>
2025-05-27 02:10:18 +0200

    progress

--- ssr-bridge/src/worker.ts
+++ ssr-bridge/src/worker.ts
@@ -1,14 +1,18 @@
 // @ts-ignore
 import { ssrTest } from "virtual:ssrBridge.js";
-import "test-dep-b";
+// @ts-ignore: won't be an issue in actual implementation
+import * as depB from "test-dep-b";
 import * as SomeUserComponent from "./SomeUserComponent";
 
 export default {
   fetch() {
     console.log("######### In worker fetch()");
 
-    // @ts-ignore
-    console.log(ssrTest(SomeUserComponent.__getID()));
+    // @ts-ignore: won't be an issue in actual implementation
+    ssrTest(SomeUserComponent.__getID());
+
+    // @ts-ignore won't be an issue in actual implementation
+    ssrTest(depB.__getID());
 
     return new Response("Hello, world!");
   },
