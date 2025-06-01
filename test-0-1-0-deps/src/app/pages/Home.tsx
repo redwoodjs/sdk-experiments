b9518305d9793d9173f3936091155f421c19e392
justinvdm <justinvderm@gmail.com>
2025-06-02 01:43:17 +0200

    progress

--- test-0-1-0-deps/src/app/pages/Home.tsx
+++ test-0-1-0-deps/src/app/pages/Home.tsx
@@ -1,9 +1,15 @@
 import { RequestInfo } from "rwsdk/worker";
+import { TestDepDualClientComponent } from "test-dep-dual";
+import { TestClient } from "./TestClient";
 
 export function Home({ ctx }: RequestInfo) {
   return (
     <div>
-      <h1>Hello World</h1>
+      <TestClient />
+      <div>
+        Used directly from server component:
+        <TestDepDualClientComponent />
+      </div>
     </div>
   );
 }
--- test-0-1-0-deps/src/app/pages/TestClient.tsx
+++ test-0-1-0-deps/src/app/pages/TestClient.tsx
@@ -1,5 +1,14 @@
 "use client";
+import { TestDepDualClientComponent } from "test-dep-dual";
 
 export function TestClient() {
-  return <div>TestClient</div>;
+  return (
+    <>
+      <div>TestClient</div>
+      <div>
+        <div>Used from another client component:</div>
+        <TestDepDualClientComponent />
+      </div>
+    </>
+  );
 }
