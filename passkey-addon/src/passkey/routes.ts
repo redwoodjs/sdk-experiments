33f889356db2cc119f30707774b4644dca91917b
justinvdm <justinvderm@gmail.com>
2025-06-19 12:37:42 +0200

    progress

--- passkey-addon/src/passkey/routes.ts
+++ passkey-addon/src/passkey/routes.ts
@@ -1,6 +1,6 @@
 import { route } from "rwsdk/router";
 import { Login } from "./components/Login.js";
-import debug from "../sdk/logger.js";
+import debug from "rwsdk/debug";
 
 const log = debug("passkey:routes");
 
