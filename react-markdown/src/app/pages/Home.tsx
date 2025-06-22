9b48056d8162d770c4aa6ef74934ff0bb89b3d82
justinvdm <justinvderm@gmail.com>
2025-06-23 01:32:51 +0200

    react markdown experiment

---
+++ react-markdown/src/app/pages/Home.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ReactMarkdown from "react-markdown";
+
+export function Home() {
+  return (
+    <div>
+      <ReactMarkdown>
+        # Hello from React Markdown! This is a **basic** example.
+      </ReactMarkdown>
+    </div>
+  );
+}
