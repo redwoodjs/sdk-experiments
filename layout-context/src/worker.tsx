297422c6ca9515bac2800952aaa5faef68658caa
justinvdm <justinvderm@gmail.com>
2025-08-21 19:24:09 +0200

    layout context experiment improvements

--- layout-context/src/worker.tsx
+++ layout-context/src/worker.tsx
@@ -2,7 +2,6 @@ import { defineApp } from "rwsdk/worker";
 import { render, route, prefix, layout } from "rwsdk/router";
 
 import { Document } from "@/app/Document";
-import { Home } from "@/app/pages/Home";
 import { setCommonHeaders } from "@/app/headers";
 import { MainLayout } from "@/app/components/layouts/MainLayout";
 import { QuestionListPage } from "@/app/pages/questions/QuestionListPage";
@@ -19,10 +18,6 @@ export default defineApp([
   setCommonHeaders(),
   ({ ctx }) => {},
   render(Document, [
-    //route("/", Home),
-    prefix("" /*"/questions"*/, [
-      layout(MainLayout, [route("/", QuestionListPage)]),
-    ]),
-    //prefix("" /*"/questions"*/, [route("/", QuestionListPage)]),
+    prefix("", [layout(MainLayout, [route("/", QuestionListPage)])]),
   ]),
 ]);
