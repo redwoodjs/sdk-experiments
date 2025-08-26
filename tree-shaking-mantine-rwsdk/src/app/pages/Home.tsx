c513ba0013b585c851f1d72f5037802f40143a40
justinvdm <justinvderm@gmail.com>
2025-08-26 20:53:59 +0200

    progress

--- tree-shaking-mantine-rwsdk/src/app/pages/Home.tsx
+++ tree-shaking-mantine-rwsdk/src/app/pages/Home.tsx
@@ -1,22 +1,16 @@
-import { MantineProvider, Button } from '@mantine/core';
+import { MantineProvider, Button } from "@mantine/core";
 
 export function Home() {
-  const handleClick = () => {
-    alert('Mantine Button clicked!');
-  };
-
   return (
     <MantineProvider>
-      <div style={{ padding: '2rem' }}>
+      <div style={{ padding: "2rem" }}>
         <h1>Tree Shaking Test - Mantine + RedwoodSDK</h1>
         <p>
           This project imports only the Button component from @mantine/core.
           Other Mantine components should be tree-shaken out of the bundle.
         </p>
-        
-        <Button onClick={handleClick} variant="filled">
-          Mantine Button
-        </Button>
+
+        <Button variant="filled">Mantine Button</Button>
       </div>
     </MantineProvider>
   );
