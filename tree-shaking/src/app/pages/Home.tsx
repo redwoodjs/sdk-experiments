427d88e4f984a31cb0ca574fa8654593dee3653b
justinvdm <justinvderm@gmail.com>
2025-08-26 04:23:02 +0200

    progress

--- tree-shaking/src/app/pages/Home.tsx
+++ tree-shaking/src/app/pages/Home.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { add } from "dummy-tree-shake-test";
+import { Button } from "dummy-tree-shake-test";
 
 export function Home() {
-  console.log("############", add(1, 2));
   return (
     <div>
-      <h1>Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
