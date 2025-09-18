81ecd79f3a9fbfc895f8dc8c1c3f4312b462e7d4
justinvdm <justinvderm@gmail.com>
2025-09-18 16:54:29 +0200

    Update client initialization to use initClient.

--- radix-ui-bug/src/client.tsx
+++ radix-ui-bug/src/client.tsx
@@ -1,11 +1,4 @@
 import "virtual:vite-preamble";
-import { hydrate } from "rwsdk/app";
+import { initClient } from "rwsdk/client";
 
-console.log("[RSDK] client.tsx execution start");
-console.log("[RSDK] DOM state:", {
-  root: document.getElementById("root"),
-  reactStateScript: document.getElementById("_R_"),
-  bodyContent: document.body.innerHTML,
-});
-
-hydrate();
+initClient();
