f07a8146beb7bfc5e4747ae9e93570109fa438da
justinvdm <justinvderm@gmail.com>
2025-09-18 14:02:53 +0200

    Fix import and style quotes.

--- radix-ui-bug/src/app/components/Counter.tsx
+++ radix-ui-bug/src/app/components/Counter.tsx
@@ -1,13 +1,15 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div style={{ border: '1px solid grey', padding: '1rem', margin: '1rem 0' }}>
+    <div
+      style={{ border: "1px solid grey", padding: "1rem", margin: "1rem 0" }}
+    >
       <h2>Client Counter</h2>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
     </div>
   );
 };
